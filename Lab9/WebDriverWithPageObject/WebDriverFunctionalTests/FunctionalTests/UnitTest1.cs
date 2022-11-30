ab9367003754c2f11836bceb1c7264536ea267f7
Polina Poznyak <polinappoznyak@gmail.com>
2022-11-30 23:54:12 +0300

    PageObject

--- Lab9/WebDriverWithPageObject/WebDriverFunctionalTests/FunctionalTests/UnitTest1.cs
+++ Lab9/WebDriverWithPageObject/WebDriverFunctionalTests/FunctionalTests/UnitTest1.cs
@@ -12,16 +12,6 @@ namespace FunctionalTests
         [Test]
         public void ICanWin()
         {
-            IWebDriver driver = new ChromeDriver("D:\\WebDrivers\\Chrome");
-            driver.Url = "https://pastebin.com";
-            driver.FindElement(By.Id("postform-text")).SendKeys("Hello from WebDriver");
-            driver.FindElement(By.Id("select2-postform-expiration-container")).Click();
-            driver.FindElement(By.XPath("//li[text()='10 Minutes']")).Click();
-            driver.FindElement(By.Id("postform-name")).SendKeys("helloweb");
-            driver.FindElement(By.XPath("//button[@class='btn -big']")).Click();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Close();
-
             StartPage start = new(driver);
             start.SendTextToInputTextArea("Hello from WebDriver");
             start.ClickSelectExpiration();
